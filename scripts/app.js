9f1865c2df02cd69a037fc36ad1f84ad0533653a
kotashovv <kotashov03@gmail.com>
2023-12-27 01:05:55 +0400

    update

--- scripts/app.js
+++ scripts/app.js
@@ -28,4 +28,21 @@ document.addEventListener('DOMContentLoaded', ()=>{
         burgerbtn.classList.add('active');
     }
 
+
+    const commandSlider = new Swiper('.command__swiper', {
+        slidesPerView: 1,
+        navigation: {
+            prevEl: '.command__prev-slide',
+            nextEl: '.command__next-slide'
+        }, 
+        breakpoints: {
+            1: {
+                slidesPerView: 1.05, 
+            },
+            920: {
+                slidesPerView: 1, 
+            }
+        }
+    })
+
 });
