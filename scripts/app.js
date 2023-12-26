3bc981c28af9adabd3be9125c304807b52aaf778
kotashovv <kotashov03@gmail.com>
2023-12-27 02:33:07 +0400

    update

--- scripts/app.js
+++ scripts/app.js
@@ -45,4 +45,51 @@ document.addEventListener('DOMContentLoaded', ()=>{
         }
     })
 
+
+    const rangeSlider = document.querySelector('#range-slider');
+
+
+    if (rangeSlider) {
+        const middleElem = document.querySelector('.range-slider .middle');
+        const finishElem = document.querySelector('.range-slider .finish');
+        noUiSlider.create(rangeSlider, {
+            start: 75,
+            connect: 'lower',
+            tooltips: true,
+            step: 1,
+            range: {
+                'min': 50,
+                'max': 250
+            },
+            format: {
+                to: function (value) {
+                    return value + 'K';
+                },
+                from: function (value) {
+                    return Number(value.replace(',-', ''));
+                }
+            },
+            
+            
+        });
+
+        rangeSlider.noUiSlider.on('update', function (values, handle) {
+            const sliderValue = parseInt(values[handle]);
+    
+            // Проверка для элемента с классом 'middle'
+            if (sliderValue >= 150) {
+                middleElem.classList.add('active');
+            } else {
+                middleElem.classList.remove('active');
+            }
+    
+            // Проверка для элемента с классом 'finish'
+            if (sliderValue === 250) {
+                finishElem.classList.add('active');
+            } else {
+                finishElem.classList.remove('active');
+            }
+        });
+    }
+
 });
